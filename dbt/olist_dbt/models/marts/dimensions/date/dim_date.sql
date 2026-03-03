ec55f6e32084ef5254c5518ac52f9919980317c9
Nagela Renata <nagelarenatanlr@gmail.com>
2026-03-03 00:50:07 +0000

    feat(dimensions): add conformed dim_date to enable consistent time-based analysis across facts

---
+++ dbt/olist_dbt/models/marts/dimensions/date/dim_date.sql
@@ -0,0 +1,34 @@
+{{ config(materialized='table') }}
+
+with calendar as (
+  -- ajuste o range se quiser
+  select
+    d as date_day
+  from unnest(generate_date_array(date('2016-01-01'), date('2019-12-31'))) as d
+)
+
+select
+  -- surrogate key simples (YYYYMMDD). ótimo pra portfólio e fácil de usar
+  cast(format_date('%Y%m%d', date_day) as int64) as date_key,
+
+  date_day,
+
+  extract(year from date_day) as year,
+  extract(quarter from date_day) as quarter,
+  extract(month from date_day) as month,
+  format_date('%B', date_day) as month_name,
+  extract(day from date_day) as day,
+
+  extract(week from date_day) as week_of_year,
+  extract(dayofweek from date_day) as day_of_week, -- 1=Sunday .. 7=Saturday (BigQuery)
+  format_date('%A', date_day) as day_name,
+
+  case when extract(dayofweek from date_day) in (1, 7) then true else false end as is_weekend,
+
+  -- úteis pra filtros
+  date_trunc(date_day, month) as month_start_date,
+  date_trunc(date_day, week) as week_start_date,
+  date_trunc(date_day, quarter) as quarter_start_date,
+  date_trunc(date_day, year) as year_start_date
+
+from calendar
