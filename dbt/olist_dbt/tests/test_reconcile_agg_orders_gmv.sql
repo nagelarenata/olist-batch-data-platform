eacbf09073925d3e70c40d2af91ff418c2e11055
Nagela Renata <nagelarenatanlr@gmail.com>
2026-03-03 18:57:24 +0000

    test(agg_orders): add GMV reconciliation test against fact_order_items

---
+++ dbt/olist_dbt/tests/test_reconcile_agg_orders_gmv.sql
@@ -0,0 +1,25 @@
+-- Fails if order-level GMV in agg_orders does not match the sum of item GMV.
+
+with items as (
+  select
+    order_id,
+    sum(item_gmv) as items_gmv
+  from {{ ref('fact_order_items') }}
+  group by 1
+),
+
+orders as (
+  select
+    order_id,
+    order_gmv
+  from {{ ref('agg_orders') }}
+)
+
+select
+  o.order_id,
+  i.items_gmv,
+  o.order_gmv,
+  (i.items_gmv - o.order_gmv) as diff
+from orders o
+join items i using (order_id)
+where abs(i.items_gmv - o.order_gmv) > 0.01
